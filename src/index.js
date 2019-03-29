fef3008d420a52fdee6e12de7fbea53e167f8de5
Dan Lynch <pyramation@gmail.com>
2019-03-29 15:39:04 -0700

    update default

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 export * from './utils';
+import { minimist } from './minimist-mod';
+import { getMinimistOpts, processArgs, processArgPaths, validateArgs } from './utils';
+
+export default (questions, ...payload) => {
+  const opts = getMinimistOpts(questions);
+  let argv = processArgs(questions, minimist(payload || [], opts));
+  processArgPaths(questions, argv);
+  validateArgs(questions, argv);
+};
