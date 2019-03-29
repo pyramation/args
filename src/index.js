2981621f820054de1a55ee15586d28c6c248aa8d
Dan Lynch <pyramation@gmail.com>
2019-03-29 16:00:43 -0700

    dont validate internally, allow user to do this

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ export * from './utils';
 export * from './minimist-mod';
 
 import { minimist } from './minimist-mod';
-import { getMinimistOpts, processArgs, processArgPaths, validateArgs } from './utils';
+import { getMinimistOpts, processArgs, processArgPaths } from './utils';
 
 export default (questions, ...payload) => {
   const opts = getMinimistOpts(questions);
   let argv = processArgs(questions, minimist(payload || [], opts));
   processArgPaths(questions, argv);
-  validateArgs(questions, argv);
+  // TODO add option for validateArgs
+  return argv;
 };
